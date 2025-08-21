bb18a8f4adca95b903a9d134012b09da6d3f0510
PAKJAWIR <ameisrain@gmail.com>
2025-08-21 20:26:20 +0700

    Compress

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
 
-// https://vite.dev/config/
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // GZIP
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 10240, // hanya file >10KB
+      deleteOriginalAssets: false,
+    }),
+    // BROTLI
+    compression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 10240,
+      deleteOriginalAssets: false,
+    }),
+  ],
+  build: {
+    minify: isProd ? "terser" : "esbuild",
+    terserOptions: {
+      compress: {
+        drop_console: true, // hapus console.log di prod
+        drop_debugger: true, // hapus debugger
+      },
+    },
+  },
 });
