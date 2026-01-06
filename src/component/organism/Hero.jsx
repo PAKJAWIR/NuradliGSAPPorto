7c0a1311b2f528ea4e5216e42ae2567cee4a61af
PAKJAWIR <ameisrain@gmail.com>
2026-01-06 18:27:18 +0700

    Speed update

--- src/component/organism/Hero.jsx
+++ src/component/organism/Hero.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -9,8 +9,12 @@ gsap.registerPlugin(ScrollTrigger);
 function Hero({ navbarRef }) {
   const container = useRef(null);
 
+  const [showSilk, setShowSilk] = useState(false);
+
   useGSAP(
     () => {
+      requestAnimationFrame(() => setShowSilk(true));
+
       const triggerBtn = ScrollTrigger.create({
         trigger: container.current,
         start: "bottom-=95 center-=50",
@@ -30,12 +34,10 @@ function Hero({ navbarRef }) {
         <div className=" relative h-full w-full flex items-center justify-center overflow-hidden">
           <div className="relative overflow-hidden flex items-center justify-center w-full h-full md:w-[1025px] md:h-[535px] rounded-none md:rounded-lg">
             {/* ===== MOBILE VIDEO ===== */}
-            <video className="absolute inset-0 w-full h-full object-cover md:hidden" src="/img/silkvd.webm" autoPlay muted loop plays-Inline />
+            <video preload="metadata" className="absolute inset-0 w-full h-full object-cover md:hidden" src="/img/silkvd.webm" autoPlay muted loop plays-Inline />
 
             {/* ===== DESKTOP SILK ===== */}
-            <div className="hidden md:block w-full h-full overflow-hidden">
-              <Silk speed={5} scale={0.9} color="#486970" noiseIntensity={1.5} rotation={0} />
-            </div>
+            <div className="hidden md:block w-full h-full overflow-hidden">{showSilk && <Silk speed={5} scale={0.9} color="#486970" noiseIntensity={1.5} rotation={0} />}</div>
           </div>
 
           <h1 className="absolute text-warna1 text-lg md:text-2xl mix-blend-difference">NURADLI</h1>
